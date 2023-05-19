3d383f5173629d77ae36c4895b07c91c09ffb915
trey_weathers <treychelle.weathers2017@gmail.com>
2023-05-19 16:13:59 -0400

    connected to tensorflow cpu backend -  may switch to Tensorflow Serving for RESTful API

--- tensorflow_cpu.js
+++ tensorflow_cpu.js
@@ -1,5 +1,6 @@
 // Import @tensorflow/tfjs-core
-//npm install @tensorflow/tfjs-core
 import * as tf from '@tensorflow/tfjs-core';
 // Adds the CPU backend to the global backend registry.
 import '@tensorflow/tfjs-backend-cpu';
+//npm install @tensorflow/tfjs-core & npm install @tensorflow/tfjs-backend-cpu
+console.log("connected to backend server!")
