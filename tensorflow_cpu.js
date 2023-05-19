fbac89fdfe67b6b8fca333e31ffbd3d99b274a46
trey_weathers <treychelle.weathers2017@gmail.com>
2023-05-19 00:18:26 -0400

    Starting TensorFlow.js cpu backend for frontend model

---
+++ tensorflow_cpu.js
@@ -0,0 +1,5 @@
+// Import @tensorflow/tfjs-core
+//npm install @tensorflow/tfjs-core
+import * as tf from '@tensorflow/tfjs-core';
+// Adds the CPU backend to the global backend registry.
+import '@tensorflow/tfjs-backend-cpu';
