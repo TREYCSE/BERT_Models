65624e3fbf715734e6d37bcc641772387afcb140
trey_weathers <treychelle.weathers2017@gmail.com>
2023-05-20 05:46:56 -0400

    saving models for conversion for TensorFlowServing

--- tensorflow_cpu.js
+++
@@ -1,6 +0,0 @@
-// Import @tensorflow/tfjs-core
-import * as tf from '@tensorflow/tfjs-core';
-// Adds the CPU backend to the global backend registry.
-import '@tensorflow/tfjs-backend-cpu';
-//npm install @tensorflow/tfjs-core & npm install @tensorflow/tfjs-backend-cpu
-console.log("connected to backend server!")
